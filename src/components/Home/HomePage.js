3b0ac146a92ad9c2c5cd2ea3682628271c7c46bd
Laithuy03th <laithuy03th@gmail.com>
2024-12-14 00:10:40 +0700

    hello

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,9 +1,16 @@
+import VideoHomePage from '../../assets/video-homepage.mp4'
+
 const HomePage = (props) =>{
     return (
        <div className="homepage-container">
-       home page component
+       <video   autoPlay muted loop  >
+       <source data-testid="currentDefaultVideo" src= {VideoHomePage}
+       type="video/mp4"/>
+       </video>
 
        </div>
     )
 }
 export default HomePage;
+
+// width="100%"  loop="" muted="" autoplay="" playsinline="" preload="metadata" x-webkit-airplay="deny" class="sc-8ad69ebd-0 fglgYn"
